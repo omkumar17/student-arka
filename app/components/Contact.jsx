0287a63d7faebd0b66286d66e2f87bfbd2c208a7
OmKumar <omkumar1870@gmail.com>
2024-11-02 22:49:40 +0530

    corrected contact error

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ContactChild from './contactChild'
+import ContactChild from './ContactChild'
 
 const Contact = () => {
   return (
--- app/student/contacts/page.js
+++ app/student/contacts/page.js
@@ -1,4 +1,4 @@
-import Contact from '@/app/components/contact'
+import Contact from '@/app/components/Contact'
 import React from 'react'
 
 const page = () => {
